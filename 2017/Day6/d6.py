2a71f663f60e0aee427e41bec52df515c83202b2
Matt Lemons <mkoler@gmail.com>
2017-12-12 18:47:11 -0800

    fix

--- 2017/Day6/d6.py
+++ 2017/Day6/d6.py
@@ -1,10 +1,13 @@
+from copy import deepcopy
+
 inp = [10,3,15,10,5,15,5,15,9,2,5,8,5,2,3,6]
 test = [0,2,7,0]
 
 def part1(mem_bank):
+    mem_bank = deepcopy(mem_bank)
     seen = set()
-    while ''.join(str(mem_bank)) not in seen:
-        seen.add(''.join(str(mem_bank)))
+    while str(mem_bank) not in seen:
+        seen.add(str(mem_bank))
         redis = max(mem_bank)
         start = mem_bank.index(redis) + 1
         mem_bank[start-1] = 0
@@ -12,8 +15,8 @@ def part1(mem_bank):
         while redis >= len(mem_bank):
             redis -= len(mem_bank)
             mem_bank = [n+1 for n in mem_bank]
-        for i in range(1,redis+1):
-            mem_bank[i-1] += 1
+        for i in range(redis):
+            mem_bank[i] += 1
         mem_bank = mem_bank[-start:] + mem_bank[:-start]
     return len(seen)
 
@@ -22,9 +25,10 @@ print(part1(test))
 print(part1(inp))
 
 def part2(mem_bank):
+    mem_bank = deepcopy(mem_bank)
     seen = []
-    while ''.join(str(mem_bank)) not in seen:
-        seen.append(''.join(str(mem_bank)))
+    while str(mem_bank) not in seen:
+        seen.append(str(mem_bank))
         redis = max(mem_bank)
         start = mem_bank.index(redis) + 1
         mem_bank[start-1] = 0
@@ -32,11 +36,12 @@ def part2(mem_bank):
         while redis >= len(mem_bank):
             redis -= len(mem_bank)
             mem_bank = [n+1 for n in mem_bank]
-        for i in range(1,redis+1):
-            mem_bank[i-1] += 1
+        for i in range(redis):
+            mem_bank[i] += 1
         mem_bank = mem_bank[-start:] + mem_bank[:-start]
-    return part1(mem_bank)
+    return len(seen) - seen.index(str(mem_bank))
 
 print(part2(test))
 
 print(part2(inp))
+
