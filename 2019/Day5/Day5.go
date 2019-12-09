f86f7359d531a1a4d49274f7e8062dae8a213898
Matt Lemons <mkoler@gmail.com>
2019-12-08 21:58:49 -0800

    day5 refactor

--- 2019/Day5/Day5.go
+++ 2019/Day5/Day5.go
@@ -12,12 +12,12 @@ import (
 func main() {
 	r := loadInput()
 	r.inputs = append(r.inputs, 1)
-	r.d5p1exec()
+	r.execOpcodes()
 	fmt.Printf("Part 1 answer: %d\n", r.outputs[len(r.outputs)-1])
 
 	r = loadInput()
 	r.inputs = append(r.inputs, 5)
-	r.d5p2exec()
+	r.execOpcodes()
 	fmt.Printf("Part 2 answer: %d\n", r.outputs[len(r.outputs)-1])
 }
 
@@ -56,63 +56,59 @@ type commands struct {
 	currentloc            int
 }
 
-func (r *commands) d5p1exec() {
+func (r *commands) execOpcodes() {
 	for r.currentloc < len(r.cmds) {
 		cmd := r.cmds[r.currentloc]
 		opcode := cmd % 100
 		modes := []int{(cmd / 100) % 10, (cmd / 1000) % 10, (cmd / 10000) % 10}
 		switch opcode {
 		case 1: // 1st + 2param put in 3rd param, move ahead 4
-			var first, second int
-			if modes[0] == 0 {
-				first = r.cmds[r.cmds[r.currentloc+1]]
-			} else {
-				first = r.cmds[r.currentloc+1]
-			}
-			if modes[1] == 0 {
-				second = r.cmds[r.cmds[r.currentloc+2]]
-			} else {
-				second = r.cmds[r.currentloc+2]
-			}
-			if modes[2] == 0 {
-				r.cmds[r.cmds[r.currentloc+3]] = first + second
-			} else {
-				r.cmds[r.currentloc+3] = first + second
-			}
+			first := r.getVal(modes[0], r.currentloc+1)
+			second := r.getVal(modes[1], r.currentloc+2)
+			r.setVal(modes[2], r.currentloc+3, first+second)
 			r.currentloc += 4
 		case 2: // 1st * 2param put in 3rd param, move ahead 4
-			var first, second int
-			if modes[0] == 0 {
-				first = r.cmds[r.cmds[r.currentloc+1]]
-			} else {
-				first = r.cmds[r.currentloc+1]
-			}
-			if modes[1] == 0 {
-				second = r.cmds[r.cmds[r.currentloc+2]]
-			} else {
-				second = r.cmds[r.currentloc+2]
-			}
-			if modes[2] == 0 {
-				r.cmds[r.cmds[r.currentloc+3]] = first * second
-			} else {
-				r.cmds[r.currentloc+3] = first * second
-			}
+			first := r.getVal(modes[0], r.currentloc+1)
+			second := r.getVal(modes[1], r.currentloc+2)
+			r.setVal(modes[2], r.currentloc+3, first*second)
 			r.currentloc += 4
 		case 3: // input
-			if modes[0] == 0 {
-				r.cmds[r.cmds[r.currentloc+1]] = r.inputs[0]
-			} else {
-				r.cmds[r.currentloc+1] = r.inputs[0]
-			}
+			r.setVal(modes[0], r.currentloc+1, r.inputs[0])
 			r.currentloc += 2
 			r.inputs = r.inputs[1:]
 		case 4: // output
-			if modes[0] == 0 {
-				r.outputs = append(r.outputs, r.cmds[r.cmds[r.currentloc+1]])
-			} else {
-				r.outputs = append(r.outputs, r.cmds[r.currentloc+1])
-			}
+			r.outputs = append(r.outputs, r.getVal(modes[0], r.currentloc+1))
 			r.currentloc += 2
+		case 5: // jump if first param is non-zero
+			if r.getVal(modes[0], r.currentloc+1) == 0 {
+				r.currentloc += 3
+				continue
+			}
+			r.currentloc = r.getVal(modes[1], r.currentloc+2)
+		case 6: // jump if first param is zero
+			if r.getVal(modes[0], r.currentloc+1) != 0 {
+				r.currentloc += 3
+				continue
+			}
+			r.currentloc = r.getVal(modes[1], r.currentloc+2)
+		case 7: // store 1 if first < second else 0
+			first := r.getVal(modes[0], r.currentloc+1)
+			second := r.getVal(modes[1], r.currentloc+2)
+			var val int
+			if first < second {
+				val = 1
+			}
+			r.setVal(modes[2], r.currentloc+3, val)
+			r.currentloc += 4
+		case 8: // store 1 if first == second else zero
+			first := r.getVal(modes[0], r.currentloc+1)
+			second := r.getVal(modes[1], r.currentloc+2)
+			var val int
+			if first == second {
+				val = 1
+			}
+			r.setVal(modes[2], r.currentloc+3, val)
+			r.currentloc += 4
 		case 99: // halt
 			r.currentloc = 1000000
 			return
@@ -123,146 +119,25 @@ func (r *commands) d5p1exec() {
 	}
 }
 
-func (r *commands) d5p2exec() {
-	for r.currentloc < len(r.cmds) {
-		cmd := r.cmds[r.currentloc]
-		opcode := cmd % 100
-		modes := []int{(cmd / 100) % 10, (cmd / 1000) % 10, (cmd / 10000) % 10}
-		switch opcode {
-		case 1: // 1st + 2param put in 3rd param, move ahead 4
-			var first, second int
-			if modes[0] == 0 {
-				first = r.cmds[r.cmds[r.currentloc+1]]
-			} else {
-				first = r.cmds[r.currentloc+1]
-			}
-			if modes[1] == 0 {
-				second = r.cmds[r.cmds[r.currentloc+2]]
-			} else {
-				second = r.cmds[r.currentloc+2]
-			}
-			if modes[2] == 0 {
-				r.cmds[r.cmds[r.currentloc+3]] = first + second
-			} else {
-				r.cmds[r.currentloc+3] = first + second
-			}
-			r.currentloc += 4
-		case 2: // 1st * 2param put in 3rd param, move ahead 4
-			var first, second int
-			if modes[0] == 0 {
-				first = r.cmds[r.cmds[r.currentloc+1]]
-			} else {
-				first = r.cmds[r.currentloc+1]
-			}
-			if modes[1] == 0 {
-				second = r.cmds[r.cmds[r.currentloc+2]]
-			} else {
-				second = r.cmds[r.currentloc+2]
-			}
-			if modes[2] == 0 {
-				r.cmds[r.cmds[r.currentloc+3]] = first * second
-			} else {
-				r.cmds[r.currentloc+3] = first * second
-			}
-			r.currentloc += 4
-		case 3: // input
-			if modes[0] == 0 {
-				r.cmds[r.cmds[r.currentloc+1]] = r.inputs[0]
-			} else {
-				r.cmds[r.currentloc+1] = r.inputs[0]
-			}
-			r.currentloc += 2
-			r.inputs = r.inputs[1:]
-		case 4: // output
-			if modes[0] == 0 {
-				r.outputs = append(r.outputs, r.cmds[r.cmds[r.currentloc+1]])
-			} else {
-				r.outputs = append(r.outputs, r.cmds[r.currentloc+1])
-			}
-			r.currentloc += 2
-		case 5: // jump if first param is non-zero
-			var jump bool
-			if modes[0] == 0 {
-				jump = r.cmds[r.cmds[r.currentloc+1]] != 0
-			} else {
-				jump = r.cmds[r.currentloc+1] != 0
-			}
-			if !jump {
-				r.currentloc += 3
-				continue
-			}
-			if modes[1] == 0 {
-				r.currentloc = r.cmds[r.cmds[r.currentloc+2]]
-			} else {
-				r.currentloc = r.cmds[r.currentloc+2]
-			}
+func (r *commands) getVal(mode, loc int) (val int) {
+	switch mode {
+	case 0:
+		return r.cmds[r.cmds[loc]]
+	case 1:
+		return r.cmds[loc]
+	default:
+		log.Fatalf("Unhandled mode %d\n", mode)
+		return -1
+	}
+}
 
-		case 6: // jump if first param is zero
-			var jump bool
-			if modes[0] == 0 {
-				jump = r.cmds[r.cmds[r.currentloc+1]] == 0
-			} else {
-				jump = r.cmds[r.currentloc+1] == 0
-			}
-			if !jump {
-				r.currentloc += 3
-				continue
-			}
-			if modes[1] == 0 {
-				r.currentloc = r.cmds[r.cmds[r.currentloc+2]]
-			} else {
-				r.currentloc = r.cmds[r.currentloc+2]
-			}
-		case 7: // store 1 if first < second else 0
-			var first, second int
-			if modes[0] == 0 {
-				first = r.cmds[r.cmds[r.currentloc+1]]
-			} else {
-				first = r.cmds[r.currentloc+1]
-			}
-			if modes[1] == 0 {
-				second = r.cmds[r.cmds[r.currentloc+2]]
-			} else {
-				second = r.cmds[r.currentloc+2]
-			}
-			var val int
-			if first < second {
-				val = 1
-			}
-			if modes[2] == 0 {
-				r.cmds[r.cmds[r.currentloc+3]] = val
-			} else {
-				r.cmds[r.currentloc+3] = val
-			}
-			r.currentloc += 4
-		case 8: // store 1 if first == second else zero
-			var first, second int
-			if modes[0] == 0 {
-				first = r.cmds[r.cmds[r.currentloc+1]]
-			} else {
-				first = r.cmds[r.currentloc+1]
-			}
-			if modes[1] == 0 {
-				second = r.cmds[r.cmds[r.currentloc+2]]
-			} else {
-				second = r.cmds[r.currentloc+2]
-			}
-			var val int
-			if first == second {
-				val = 1
-			}
-			if modes[2] == 0 {
-				r.cmds[r.cmds[r.currentloc+3]] = val
-			} else {
-				r.cmds[r.currentloc+3] = val
-			}
-			r.currentloc += 4
-		case 99: // halt
-			r.currentloc = 1000000
-			return
-		default:
-			fmt.Printf("Unhandled cmd case %d\n", opcode)
-			return
-		}
+func (r *commands) setVal(mode, loc, val int) {
+	switch mode {
+	case 0:
+		r.cmds[r.cmds[loc]] = val
+	case 1:
+		r.cmds[loc] = val
+	default:
+		log.Fatalf("Unhandled mode %d\n", mode)
 	}
 }
